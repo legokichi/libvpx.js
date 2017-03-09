50202f6dadc2d3e7b20cf889551832fd7ee73a88
legokichi <legokichi@gmail.com>
2017-03-09 16:36:16 +0900

    add demo

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 cd libvpx
-#emmake make clean > /dev/null 2>&1
-#git reset --hard
-#patch -p1 -i ../configure.patch
+emmake make clean > /dev/null 2>&1
+git reset --hard
+patch -p1 -i ../configure.patch
 emconfigure ./configure \
-  --disable-optimizations \
   --disable-runtime-cpu-detect \
   --disable-examples \
   --disable-docs \
@@ -15,26 +14,24 @@ emconfigure ./configure \
   --disable-encode_perf_tests \
   --disable-decode_perf_tests \
   --target=asmjs-unknown-emscripten \
-  --extra-cflags="-g -O0"
-# --extra-cflags="-02"
+  --extra-cflags="-02"
+# --extra-cflags="-g -O0"
+# --disable-optimizations \
 emmake make
-#exported_functions=$(node --eval "console.log(JSON.stringify(require('../exported_functions.json')).split('\"').join('\''))")
-# とりあえず side module でコンパイルしてみる
-#emcc \
-#  -O2 \
-#  --memory-init-file 0 \
-#  -s EXPORTED_FUNCTIONS="$exported_functions" \
-#  -s SIDE_MODULE=1
-#  libvpx.a \
-#  -o ../libvpx.js
-# or -s ALLOW_MEMORY_GROWTH=1
+exported_functions=$(node --eval "console.log(JSON.stringify(require('../exported_functions.json')).split('\"').join('\''))")
 \cp -f ../src/decode.c ./
 emcc \
-  -s EXPORTED_FUNCTIONS="['_init', '_decode', '_destroy']" \
-  -g \
-  -g4 \
+  -s TOTAL_MEMORY=67108864 \
+  -O2 \
+  -s EXPORTED_FUNCTIONS="$exported_functions" \
   -I. \
   ./decode.c libvpx.a \
   -o ../decode.js 
+# -g -g4 : デバッグ
+# -O2 : 最適化
+# --memory-init-file 0 : .js.mem 吐かない
+# -s ALLOW_MEMORY_GROWTH=1 : 可変ヒープ
+# -s TOTAL_MEMORY=X : ヒープ指定
+# -s SIDE_MODULE=1
 echo "fin"
 cd ..
--- src/decode.c
+++ src/decode.c
@@ -23,12 +23,50 @@ int init(int argc, char **argv) {
   return 0;
 }
 
+#define CM ((vpx_codec_alg_priv_t *)codec.priv)->yv12_frame_buffers.pbi[0]->common
+int show_frame(){ return CM.show_frame; }
+int width(){ return CM.Width; }
+int height(){ return CM.Height; }
+int mb_rows(){ return CM.mb_rows; }
+int mb_cols(){ return CM.mb_cols; }
+int mode_info_stride(){ return CM.mode_info_stride; }
+#define MI(NAME, OP) \
+  NAME (int row, int col){ \
+    const int idx = row * CM.mode_info_stride + col; \
+    MODE_INFO *mi = &CM.mi[idx]; \
+    return (OP); \
+  }
+
+int MI(mbmi_ref_frame, mi->mbmi.ref_frame)
+int MI(mbmi_mode, mi->mbmi.mode)
+int MI(mbmi_partitioning, mi->mbmi.partitioning)
+int MI(mbmi_uv_mode, mi->mbmi.uv_mode)
+int MI(mbmi_is_4x4, mi->mbmi.is_4x4)
+int MI(mbmi_row, mi->mbmi.mv.as_mv.row)
+int MI(mbmi_col, mi->mbmi.mv.as_mv.col)
+
+int splitmv_row (int row, int col, int i){
+  const int idx = row * CM.mode_info_stride + col;
+  MODE_INFO *mi = &((vpx_codec_alg_priv_t *)codec.priv)->yv12_frame_buffers.pbi[0]->common.mi[idx];
+  return mi->bmi[i].mv.as_mv.row;
+}
+
+int splitmv_col (int row, int col, int i){
+  const int idx = row * CM.mode_info_stride + col;
+  MODE_INFO *mi = &((vpx_codec_alg_priv_t *)codec.priv)->yv12_frame_buffers.pbi[0]->common.mi[idx];
+  return mi->bmi[i].mv.as_mv.col;
+}
+
 int decode(const unsigned char *frame, size_t frame_size){
   printf("frame_size: %d \n",(unsigned int)frame_size);
   if (vpx_codec_decode(&codec, frame, (unsigned int)frame_size, NULL, 0)){
     die_codec(&codec, "Failed to decode frame.");
   }
+  return 0;
+}
 
+
+int show(){
   // struct vpx_codec_alg_priv @ vp8/vp8_dx_iface.c
   // vp8_decode(vpx_codec_alg_priv_t *ctx, ...)'s ctx
   vpx_codec_alg_priv_t *ctx = (vpx_codec_alg_priv_t *)codec.priv;
@@ -37,25 +75,55 @@ int decode(const unsigned char *frame, size_t frame_size){
   VP8D_COMP *pbi = ctx->yv12_frame_buffers.pbi[0];
   VP8_COMMON *cm = &pbi->common;
 
-  printf("width: %d, height: %d\n", cm->Width, cm->Height);
-  printf("rows: %d, cols: %d\n", cm->mb_rows, cm->mb_cols);
-  if(!cm->show_frame){
-    printf("=========");
-    return 0;
-  }
   for (int row = 0; row < cm->mb_rows; ++row) {
     for (int col = 0; col < cm->mb_cols; ++col) {
-      const int i = row * cm->mode_info_stride + col;
-      MODE_INFO *mi = &cm->mi[i];
-      const uint8_t mode = mi->mbmi.mode; // MB_PREDICTION_MODE
+      const int idx = row * cm->mode_info_stride + col;
+      MODE_INFO *mi = &cm->mi[idx];
+      printf("(%d, %d)", row, col);
+      printf(", ");
+      switch(mi->mbmi.ref_frame){
+        case INTRA_FRAME: printf("ref_frame: %s", "INTRA_FRAME"); break;
+        case LAST_FRAME: printf("ref_frame: %s", "LAST_FRAME"); break;
+        case GOLDEN_FRAME: printf("ref_frame: %s", "GOLDEN_FRAME"); break;
+        case ALTREF_FRAME: printf("ref_frame: %s", "ALTREF_FRAME"); break;
+        case MAX_REF_FRAMES: printf("ref_frame: %s","MAX_REF_FRAMES"); break;
+      }
+      printf(", ");
+      switch(mi->mbmi.mode){
+        case DC_PRED: printf("mode: %s", "DC_PRED"); break;
+        case V_PRED: printf("mode: %s", "V_PRED"); break;
+        case H_PRED: printf("mode: %s", "H_PRED"); break;
+        case TM_PRED: printf("mode: %s", "TM_PRED"); break;
+        case B_PRED: printf("mode: %s", "B_PRED"); break;
+        case NEARESTMV: printf("mode: %s", "NEARESTMV"); break;
+        case NEARMV: printf("mode: %s", "NEARMV"); break;
+        case ZEROMV: printf("mode: %s", "ZEROMV"); break;
+        case NEWMV: printf("mode: %s", "NEWMV"); break;
+        case SPLITMV: printf("mode: %s", "SPLITMV"); break;
+        case MB_MODE_COUNT: printf("mode: %s", "MB_MODE_COUNT"); break;
+      }
+      printf(", ");
+      printf("partitioning: %d", mi->mbmi.partitioning);
+      printf(", ");
+      //mi->mbmi.uv_mode;
+      //mi->mbmi.is_4x4;
 
+      if(mi->mbmi.mode == SPLITMV) {
+        // Block Mode Info (SplitMV)
+        for(int i=0; i<16; i++){
+          // keyframe => B_PREDICTION_MODE as_mode;, !keyframe, int_mv mv;
+          const short _row = mi->bmi[i].mv.as_mv.row;
+          const short _col = mi->bmi[i].mv.as_mv.col;
+          printf("Block: %d, mv: (%d, %d)\n", i, _row, _col);
+        }
+        break;
+      }
+      // Macroblock Mode Info      
+      // 16x16 MB MV
       const short _row = mi->mbmi.mv.as_mv.row;
       const short _col = mi->mbmi.mv.as_mv.col;
-      const uint8_t is_4x4 = mi->mbmi.is_4x4;
-      printf("mode: %d, row: %d, col: %d, mv: (%d, %d), is_4x4: %d\n", mode, row, col, _row, _col, is_4x4);
-      for(int i=0; i<16; i++){
-        mi->bmi[i];// keyframe => B_PREDICTION_MODE as_mode;, !keyframe, int_mv mv;
-      }
+      
+      printf("mv: (%d, %d)\n", _row, _col);
     }
   }
 
